8194914a174dedec353384a11ced20682203e21f
swordpartee <swordpartee@gmail.com>
2026-05-20 13:04:05 -0400

    created ComponentView

--- test/core/entities/ComponentManagerTest.cpp
+++ test/core/entities/ComponentManagerTest.cpp
@@ -53,8 +53,8 @@ TEST_F(ComponentManagerTest, ComponentData) {
     auto entity = entityManager.generateEntity();
     DataComponent data{42, 3.14f};
 
-    auto &retrieved =
-        componentManager.addComponent<DataComponent>(entity, data);
+    componentManager.addComponent<DataComponent>(entity, data);
+    auto &retrieved = componentManager.getComponent<DataComponent>(entity);
 
     EXPECT_EQ(retrieved.value, 42);
     EXPECT_FLOAT_EQ(retrieved.data, 3.14f);
@@ -118,7 +118,8 @@ TEST_F(ComponentManagerTest, MultipleEntitiesWithComponents) {
 TEST_F(ComponentManagerTest, ComponentDataModification) {
     auto entity = entityManager.generateEntity();
 
-    auto &comp = componentManager.addComponent<DataComponent>(entity);
+    componentManager.addComponent<DataComponent>(entity);
+    auto &comp = componentManager.getComponent<DataComponent>(entity);
     EXPECT_EQ(comp.value, 0);
     EXPECT_FLOAT_EQ(comp.data, 0.0f);
 
@@ -169,4 +170,135 @@ TEST_F(ComponentManagerTest, ThreeComponentTypes) {
     EXPECT_EQ(retrievedData.value, 42);
     EXPECT_FLOAT_EQ(retrievedPos.x, 1.0f);
     EXPECT_FLOAT_EQ(retrievedVel.vx, 0.1f);
+}
+
+TEST_F(ComponentManagerTest, ViewSingleComponent) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+    auto entity3 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {1, 1.0f});
+    componentManager.addComponent<DataComponent>(entity2, {2, 2.0f});
+    componentManager.addComponent<DataComponent>(entity3, {3, 3.0f});
+
+    auto view = componentManager.viewComponents<DataComponent>();
+
+    EXPECT_EQ(view.size(), 3);
+    EXPECT_EQ(std::get<0>(view[0]), entity1);
+    EXPECT_EQ(std::get<1>(view[0]).value, 1);
+    EXPECT_EQ(std::get<1>(view[1]).value, 2);
+    EXPECT_EQ(std::get<1>(view[2]).value, 3);
+}
+
+TEST_F(ComponentManagerTest, ViewMultipleComponents) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {10, 1.0f});
+    componentManager.addComponent<PositionComponent>(entity1, {1.0f, 2.0f, 3.0f});
+
+    componentManager.addComponent<DataComponent>(entity2, {20, 2.0f});
+    componentManager.addComponent<PositionComponent>(entity2, {4.0f, 5.0f, 6.0f});
+
+    auto view = componentManager.viewComponents<DataComponent, PositionComponent>();
+
+    EXPECT_EQ(view.size(), 2);
+    EXPECT_EQ(std::get<1>(view[0]).value, 10);
+    EXPECT_FLOAT_EQ(std::get<2>(view[0]).x, 1.0f);
+    EXPECT_EQ(std::get<1>(view[1]).value, 20);
+    EXPECT_FLOAT_EQ(std::get<2>(view[1]).x, 4.0f);
+}
+
+TEST_F(ComponentManagerTest, ViewFiltersEntitiesWithoutAllComponents) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+    auto entity3 = entityManager.generateEntity();
+
+    // entity1 has both components
+    componentManager.addComponent<DataComponent>(entity1, {1, 1.0f});
+    componentManager.addComponent<PositionComponent>(entity1, {1.0f, 2.0f, 3.0f});
+
+    // entity2 has only DataComponent
+    componentManager.addComponent<DataComponent>(entity2, {2, 2.0f});
+
+    // entity3 has both components
+    componentManager.addComponent<DataComponent>(entity3, {3, 3.0f});
+    componentManager.addComponent<PositionComponent>(entity3, {4.0f, 5.0f, 6.0f});
+
+    auto view = componentManager.viewComponents<DataComponent, PositionComponent>();
+
+    EXPECT_EQ(view.size(), 2);
+    EXPECT_EQ(std::get<1>(view[0]).value, 1);
+    EXPECT_EQ(std::get<1>(view[1]).value, 3);
+}
+
+TEST_F(ComponentManagerTest, ViewEmptyWhenNoEntitiesMatch) {
+    auto entity1 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {1, 1.0f});
+
+    auto view = componentManager.viewComponents<DataComponent, PositionComponent>();
+
+    EXPECT_EQ(view.size(), 0);
+}
+
+TEST_F(ComponentManagerTest, ViewThreeComponentTypes) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {100, 1.0f});
+    componentManager.addComponent<PositionComponent>(entity1, {1.0f, 2.0f, 3.0f});
+    componentManager.addComponent<VelocityComponent>(entity1, {0.1f, 0.2f, 0.3f});
+
+    componentManager.addComponent<DataComponent>(entity2, {200, 2.0f});
+    componentManager.addComponent<PositionComponent>(entity2, {4.0f, 5.0f, 6.0f});
+    componentManager.addComponent<VelocityComponent>(entity2, {0.4f, 0.5f, 0.6f});
+
+    auto view = componentManager.viewComponents<DataComponent, PositionComponent, VelocityComponent>();
+
+    EXPECT_EQ(view.size(), 2);
+    EXPECT_EQ(std::get<1>(view[0]).value, 100);
+    EXPECT_FLOAT_EQ(std::get<2>(view[0]).x, 1.0f);
+    EXPECT_FLOAT_EQ(std::get<3>(view[0]).vx, 0.1f);
+}
+
+TEST_F(ComponentManagerTest, ViewAfterRemovingComponent) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {1, 1.0f});
+    componentManager.addComponent<PositionComponent>(entity1, {1.0f, 2.0f, 3.0f});
+
+    componentManager.addComponent<DataComponent>(entity2, {2, 2.0f});
+    componentManager.addComponent<PositionComponent>(entity2, {4.0f, 5.0f, 6.0f});
+
+    auto view1 = componentManager.viewComponents<DataComponent, PositionComponent>();
+    EXPECT_EQ(view1.size(), 2);
+
+    componentManager.removeComponent<PositionComponent>(entity1);
+
+    auto view2 = componentManager.viewComponents<DataComponent, PositionComponent>();
+    EXPECT_EQ(view2.size(), 1);
+    EXPECT_EQ(std::get<1>(view2[0]).value, 2);
+}
+
+TEST_F(ComponentManagerTest, ViewConsistencyWithModifications) {
+    auto entity1 = entityManager.generateEntity();
+    auto entity2 = entityManager.generateEntity();
+
+    componentManager.addComponent<DataComponent>(entity1, {1, 1.0f});
+    componentManager.addComponent<PositionComponent>(entity1, {1.0f, 2.0f, 3.0f});
+
+    componentManager.addComponent<DataComponent>(entity2, {2, 2.0f});
+    componentManager.addComponent<PositionComponent>(entity2, {4.0f, 5.0f, 6.0f});
+
+    // Modify component through getComponent (after all additions to avoid invalidating references)
+    componentManager.getComponent<DataComponent>(entity1).value = 999;
+
+    // Verify modification persists in storage
+    EXPECT_EQ(componentManager.getComponent<DataComponent>(entity1).value, 999);
+
+    // Get a fresh view after modification
+    auto view = componentManager.viewComponents<DataComponent, PositionComponent>();
+    EXPECT_EQ(std::get<1>(view[0]).value, 999);
 }
