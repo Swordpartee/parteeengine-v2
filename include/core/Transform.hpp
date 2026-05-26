63f5c70153e7d2fd5b6cfcbc7034348eb73b873c
swordpartee <swordpartee@gmail.com>
2026-05-26 10:57:40 -0400

    created transform

---
+++ include/core/Transform.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "util/Vector3.hpp"
+
+namespace util
+{
+    struct Transform {
+       Vector3 position;
+       Vector3 rotation;
+       Vector3 scale;
+    
+    }; 
+} // namespace util
---
+++ include/util/Vector3.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace util
+{
+    class Vector3 {
+    private:
+       float x, y, z;
+
+    public:
+
+    Vector3(float x, float y, float z = 0.f) : x(x), y(y), z(z) {}
+
+    }; 
+} // namespace util
