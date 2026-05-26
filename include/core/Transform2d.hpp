64263d4ae299971fa5aa7a5aae7ac8ab15a20ae7
swordpartee <swordpartee@gmail.com>
2026-05-26 17:15:42 -0400

    fix linter errors

--- include/core/Transform2d.hpp
+++ include/core/Transform2d.hpp
@@ -2,12 +2,10 @@
 
 #include "util/Vector2.hpp"
 
-namespace util
-{
-    struct Transform {
-       Vector2 position;
-       double rotation;
-       Vector2 scale;
-    
-    }; 
+namespace util {
+struct Transform {
+    Vector2 position;
+    double rotation;
+    Vector2 scale;
+};
 } // namespace util
--- include/util/Vector2.hpp
+++ include/util/Vector2.hpp
@@ -1,14 +1,12 @@
 #pragma once
 
-namespace util
-{
-    class Vector2 {
-    private:
-       float x, y, z;
+namespace util {
+class Vector2 {
+  private:
+    float x, y, z;
 
-    public:
-
-    Vector2(float x, float y) : x(x), y(y) {}
-
-    }; 
+  public:
+    Vector2(float x, float y) : x(x), y(y) {
+    }
+};
 } // namespace util
