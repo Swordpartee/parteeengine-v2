afadc4ed43f2a315dc796c52e4a181b01bffc973
swordpartee <swordpartee@gmail.com>
2026-07-20 17:12:56 -0400

    created engine class, renamed eventbus to eventmanager and moved suberscribre to the detail namespace

---
+++ test/core/EngineTest.cpp
@@ -0,0 +1,54 @@
+
+#include <gtest/gtest.h>
+
+#include "core/Engine.hpp"
+#include "core/modules/ModuleBase.hpp"
+
+namespace parteeengine {
+namespace {
+
+class TestModule : public ModuleBase {
+  public:
+    int value = 0;
+};
+
+} // namespace
+
+TEST(EngineTest, FacadeRoutesEntityAndComponentOperations) {
+    Engine engine;
+
+    auto entity = engine.createEntity();
+    ASSERT_TRUE(engine.isValidEntity(entity));
+
+    engine.addComponent(entity, 42);
+    EXPECT_TRUE(engine.hasComponent<int>(entity));
+    EXPECT_EQ(engine.getComponent<int>(entity), 42);
+
+    engine.removeComponent<int>(entity);
+    EXPECT_FALSE(engine.hasComponent<int>(entity));
+}
+
+TEST(EngineTest, FacadeRoutesEventsAndModules) {
+    Engine engine;
+
+    bool called = false;
+    int received = 0;
+
+    engine.subscribe<int>([&](const int value) {
+        called = true;
+        received = value;
+    });
+
+    engine.emit<int>(7);
+
+    EXPECT_TRUE(called);
+    EXPECT_EQ(received, 7);
+
+    engine.addModule<TestModule>();
+    auto &module = engine.getModule<TestModule>();
+    module.value = 11;
+
+    EXPECT_EQ(module.value, 11);
+}
+
+} // namespace parteeengine
---
+++ test/core/events/EventManagerTest.cpp
@@ -0,0 +1,163 @@
+#include <gtest/gtest.h>
+
+#include "core/events/EventManager.hpp"
+
+class EventManagerTest : public ::testing::Test {
+  protected:
+    parteeengine::EventManager EventManager;
+
+    struct TestEvent {};
+    struct TestEventWithData {
+        int value;
+        std::string message;
+    };
+    struct AnotherEvent {
+        double data;
+    };
+};
+
+// Basic subscription and emission test
+TEST_F(EventManagerTest, Subscribe) {
+    bool called = false;
+
+    EventManager.subscribe<TestEvent>([&called](const TestEvent) {
+        called = true;
+    });
+
+    EventManager.emit<TestEvent>({});
+
+    EXPECT_TRUE(called);
+}
+
+// Multiple subscribers to the same event type
+TEST_F(EventManagerTest, MultipleSubscribers) {
+    int callCount = 0;
+
+    EventManager.subscribe<TestEvent>([&callCount](const TestEvent) {
+        callCount++;
+    });
+    EventManager.subscribe<TestEvent>([&callCount](const TestEvent) {
+        callCount++;
+    });
+    EventManager.subscribe<TestEvent>([&callCount](const TestEvent) {
+        callCount++;
+    });
+
+    EventManager.emit<TestEvent>({});
+
+    EXPECT_EQ(callCount, 3);
+}
+
+// Different event types don't interfere
+TEST_F(EventManagerTest, DifferentEventTypes) {
+    bool testEventCalled = false;
+    bool anotherEventCalled = false;
+
+    EventManager.subscribe<TestEvent>([&testEventCalled](const TestEvent) {
+        testEventCalled = true;
+    });
+    EventManager.subscribe<AnotherEvent>([&anotherEventCalled](const AnotherEvent) {
+        anotherEventCalled = true;
+    });
+
+    EventManager.emit<TestEvent>({});
+
+    EXPECT_TRUE(testEventCalled);
+    EXPECT_FALSE(anotherEventCalled);
+}
+
+// Events with data are properly transmitted
+TEST_F(EventManagerTest, EventWithData) {
+    TestEventWithData receivedEvent = {0, ""};
+
+    EventManager.subscribe<TestEventWithData>([&receivedEvent](const TestEventWithData event) {
+        receivedEvent = event;
+    });
+
+    TestEventWithData emittedEvent = {42, "test message"};
+    EventManager.emit<TestEventWithData>(emittedEvent);
+
+    EXPECT_EQ(receivedEvent.value, 42);
+    EXPECT_EQ(receivedEvent.message, "test message");
+}
+
+// Emitting multiple times to same subscribers
+TEST_F(EventManagerTest, EmitMultipleTimes) {
+    int callCount = 0;
+
+    EventManager.subscribe<TestEvent>([&callCount](const TestEvent) {
+        callCount++;
+    });
+
+    EventManager.emit<TestEvent>({});
+    EventManager.emit<TestEvent>({});
+    EventManager.emit<TestEvent>({});
+
+    EXPECT_EQ(callCount, 3);
+}
+
+// Subscribers can be added after emission
+TEST_F(EventManagerTest, SubscribeAfterEmit) {
+    int callCount = 0;
+
+    EventManager.emit<TestEvent>({});
+
+    EventManager.subscribe<TestEvent>([&callCount](const TestEvent) {
+        callCount++;
+    });
+
+    EventManager.emit<TestEvent>({});
+
+    EXPECT_EQ(callCount, 1);
+}
+
+// Multiple different event types with different data
+TEST_F(EventManagerTest, MultipleEventTypesWithData) {
+    TestEventWithData receivedTestEvent = {0, ""};
+    AnotherEvent receivedAnotherEvent = {0.0};
+
+    EventManager.subscribe<TestEventWithData>([&receivedTestEvent](const TestEventWithData event) {
+        receivedTestEvent = event;
+    });
+    EventManager.subscribe<AnotherEvent>([&receivedAnotherEvent](const AnotherEvent event) {
+        receivedAnotherEvent = event;
+    });
+
+    TestEventWithData testEvent = {99, "multi-event test"};
+    AnotherEvent anotherEvent = {3.14159};
+
+    EventManager.emit<TestEventWithData>(testEvent);
+    EventManager.emit<AnotherEvent>(anotherEvent);
+
+    EXPECT_EQ(receivedTestEvent.value, 99);
+    EXPECT_EQ(receivedTestEvent.message, "multi-event test");
+    EXPECT_DOUBLE_EQ(receivedAnotherEvent.data, 3.14159);
+}
+
+// Emitting with no subscribers doesn't cause issues
+TEST_F(EventManagerTest, EmitWithNoSubscribers) {
+    // Should not throw or crash
+    EXPECT_NO_THROW(EventManager.emit<TestEvent>({}));
+    EXPECT_NO_THROW(EventManager.emit<TestEventWithData>({0, ""}));
+}
+
+// Multiple subscribers receive the same event data
+TEST_F(EventManagerTest, MultipleSubscribersReceiveSameData) {
+    int value1 = 0, value2 = 0, value3 = 0;
+
+    EventManager.subscribe<TestEventWithData>([&value1](const TestEventWithData event) {
+        value1 = event.value;
+    });
+    EventManager.subscribe<TestEventWithData>([&value2](const TestEventWithData event) {
+        value2 = event.value;
+    });
+    EventManager.subscribe<TestEventWithData>([&value3](const TestEventWithData event) {
+        value3 = event.value;
+    });
+
+    EventManager.emit<TestEventWithData>({123, "shared data"});
+
+    EXPECT_EQ(value1, 123);
+    EXPECT_EQ(value2, 123);
+    EXPECT_EQ(value3, 123);
+}
